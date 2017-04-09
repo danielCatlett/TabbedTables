38c84b3eaa3080ed6600ef808b1ac07aa8f42a05
danielCatlett <daniel.catlett42@gmail.com>
2017-04-08 21:15:58 -0500

    Initial Commit

---
+++ TabbedTables/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabbedTables
+//
+//  Created by Daniel Catlett on 4/8/17.
+//  Copyright © 2017 Daniel Catlett. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
